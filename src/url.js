71130ac3abcd684fbbd3aa46b41e9d02400476b0
pcmagas <pcmagas@disroot.org>
2024-05-24 17:42:13 +0300

    ES-39 Add url validation Lib

--- src/url.js
+++ src/url.js
@@ -42,9 +42,33 @@ function updateQueryParam(key, value) {
     window.history.replaceState({}, document.title, `${currentUrl.pathname}?${params.toString()}`);
 }
 
+/**
+ * Checks if a given string is a valid HTTP or HTTPS URL.
+ * 
+ * It is based upon:
+ * https://stackoverflow.com/a/3809435/4706711
+ * 
+ * @param {string} str - The string to be checked.
+ * @returns {boolean} - Returns true if the string is a valid HTTP or HTTPS URL, false otherwise.
+ */
+function isValidHttpUrl(str) {
+    const pattern = new RegExp(
+        '^(https?:\\/\\/)?' + // Optional protocol (http or https)
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // Domain name (e.g., example.com)
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IPv4 address (e.g., 192.168.0.1)
+        '(\\:\\d+)?' + // Optional port (e.g., :8080)
+        '(\\/[-a-z\\d%_.~+]*)*' + // Optional path (e.g., /path/to/resource)
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // Optional query string (e.g., ?key=value)
+        '(\\#[-a-z\\d_]*)?$', // Optional fragment (e.g., #section)
+        'i' // Case-insensitive flag
+    );
+    return pattern.test(str);
+}
+
 export {
     prependHttps,
     extractHostnameFromUrl,
     getHostnameWithoutExtention,
-    updateQueryParam
+    updateQueryParam,
+    isValidHttpUrl
 }
