264623db9444a8349846665527ac0a2df52ceba0
pcmagas <pcmagas@disroot.org>
2024-07-10 11:55:09 +0300

    Generate exports upon release

--- release.sh
+++ release.sh
@@ -89,6 +89,7 @@ new_version=$(npm version ${new_version} --no-git-tag-version --no-commit-hooks 
 echo "Version bumped to $new_version"
 
 echo "Keep package-lock.json up to spec"
+npm run generate-exports
 npm install
 
 git commit -m "[Auto script] AutoBump Version to ${new_version}" package.json package-lock.json
