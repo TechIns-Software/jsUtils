07f6a8c70e6ce84afad9d6ced15018fd20e6b95d
pcmagas <pcmagas@disroot.org>
2024-07-10 13:14:47 +0300

    AO-51 Use function for input error

--- src/input-error.js
+++ src/input-error.js
@@ -76,8 +76,45 @@ function displayExistingErrorMessage(inputElem){
     }
 }
 
+/**
+ * Handles form submission errors by processing the response in a specific JSON format.
+ * The response should contain a `msg` field, which can be either a string or an object.
+ *
+ * - If the response status is 400, `msg` should be an object where:
+ *   - The keys are column names (corresponding to form input names).
+ *   - The values are string of error messages for the respective inputs.
+ *   - Example: 
+ *       { msg: { "username": "Username is required", "email": "Invalid email address" }}
+ *
+ *   The function will append a Bootstrap input error message to the respective input elements.
+ *
+ * - For other statuses, `msg` should be a string containing the error message. For example:
+ *     { "msg": "An unexpected error occurred" }
+ *
+ * @param {Object} xhr - The XMLHttpRequest response object.
+ * @param {Function} next - The callback function to be called for further handling.
+ *                           The function is called with the following arguments:
+ *                           - {boolean} hasErrors - Indicates if there were validation errors (true for status 400, false otherwise).
+ *                           - {Object|string} msg - The error messages object or string from the response.
+ *                           - {Object} xhr - The original XMLHttpRequest response object.
+ */
+function errorResponseHandler(xhr,next){
+    const responseJson = JSON.parse(xhr.responseText)['msg']
+    if(xhr.status == 400){
+        Object.keys(responseJson).forEach((key)=>{
+            const msg = responseJson[key].join("<br>")
+            addInputErrorMsg(document.querySelector(`input[name=${key}]`),msg)
+        })
+        next(true,responseJson,xhr);
+        return
+    }
+
+    next(false,responseJson,xhr)
+}
+
 export {
     addInputErrorMsg,
     clearInputErrorMessage,
-    displayExistingErrorMessage
+    displayExistingErrorMessage,
+    errorResponseHandler
 }
