906d15bfeddc4970f703370acc53c5e76bfb2405
pcmagas <pcmagas@disroot.org>
2024-09-20 12:00:02 +0300

    Document Function Better

--- src/modal.js
+++ src/modal.js
@@ -8,16 +8,23 @@ import { clearInputErrorMessage } from "./input-error";
  *
  * @param {HTMLElement|string} modalElem - The modal element (or its selector) containing the form.
  * 
- * @param {function(HTMLFormElement, Object|string, Object):void} submitSuccessCallback - Callback function to execute on successful form submission.
+ * @param {function(HTMLFormElement, Object|string, Modal):void} submitSuccessCallback - Callback function to execute on successful form submission.
  * - First Argument is the form Element that resides inside the modal
  * - The Second one is the Data receved from ajax
- * - The thirsd one is the Bootstrap Modal
+ * - The third one is the Bootstrap Modal
  * 
  * @param {function(boolean, boolean, Object, XMLHttpRequest):void} submitFailureCallback - Callback function to execute on form submission failure.
- *  First argument is whether ajax has performed, second is if it’s a 400 error, third is response data, fourth is the XMLHttpRequest.
- * @param {function(XMLHttpRequest):void} [beforeSend] - Optional callback function to execute before the form is sent.
+ * 
+ *   - First argument is whether ajax has performed, 
+ *   - Second is if it’s a 400 error, 
+ *   - third is response data. The ajax data should contain at least a `msg` element containing the error message 
+ *   - fourth is the XMLHttpRequest.
+ * 
+ * @param {function(XMLHttpRequest):void} [beforeSend] - Optional callback function to execute before the form is sent. Arguments are the same used upon submitFormAjax
+ * 
  * @param {function(Event, HTMLFormElement, HTMLElement, Object, function(Event):void):void} [onSubmitHandle] - Optional callback function to handle form submission.
  *  This function should receive event, form element, modal element, modal instance, and a callback to trigger form submission.
+ * 
  * @param {function(HTMLElement, Object, HTMLFormElement):void} [onModalClose] - Optional callback to execute when the modal is closed.
  */
 function submitFormUponModalUsingAjax(modalElem,submitSuccessCallback,submitFailureCallback,beforeSend,onSubmitHandle,onModalClose) {
@@ -35,7 +42,7 @@ function submitFormUponModalUsingAjax(modalElem,submitSuccessCallback,submitFail
         }
     });
 
-
+    // Callback function upon submit
     const formSubmitAjaxCallback = (e)=>{
         submitFormAjax(form,(data)=>submitSuccessCallback(form,data,modal),(jqxhr)=>{
             errorResponseHandler(jqxhr,(is400,responseJson,xhr)=>{
@@ -55,6 +62,8 @@ function submitFormUponModalUsingAjax(modalElem,submitSuccessCallback,submitFail
         e.stopPropagation();
 
         if(typeof onSubmitHandle === 'function'){
+            // I do not know whether `onSubmitHandle` contains Syncronous or asyncronous function 
+            // Thus I use promise to always ensure that ajax call is submited 
             Promise.resolve(onSubmitHandle(e, form, modalElem, modal)).then(()=>{
                 formSubmitAjaxCallback(e);
             }).catch((error)=>{
